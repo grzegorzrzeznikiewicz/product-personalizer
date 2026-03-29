ec56ec4248841f2882ce8a47a4f6b66bb58fa1f3
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-03-29 22:10:21 +0200

    Submodule 15.3: etap3

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,6 +7,9 @@ import {useState} from "react";
 const Product = props => {
   const [currentColor] = useState(props.colors[0]);
   const [currentSize] = useState(props.sizes[0].name);
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
 
   return (
     <article className={styles.product}>
@@ -25,18 +28,23 @@ const Product = props => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map((size) => (
+                  <li key={size.name}>
+                    <button type="button" className={clsx(size.name === currentSize && styles.active)}>
+                      {size.name}
+                    </button>
+                  </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map((color) => (
+                  <li key={color}>
+                    <button type="button" className={clsx(prepareColorClassName(color), color === currentColor && styles.active)} />
+                  </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
